32704e50b026b00c47899dc41c10f32932af94b2
Akhaled19 <akhaled@clarku.edu>
2020-02-02 23:55:17 -0500

    deleted the errors section from app.js

--- app.js
+++ app.js
@@ -39,29 +39,3 @@ app.listen(3000, () => {
 
 
                         /*Handle Errors*/
-//404 error
-    // if a user navigates to a non-existing route 
-    app.use( (req, res, next) => {
-        //display a user friendly message 
-        const err = new Error('Not Found');
-        err.status = 404;
-        next(err);
-    });    
-
-//app.js error handler 
-app.use( (err, req, res, next) => { 
-    // sets the error message to a user friendly message 
-    res.locals.error = err;
-    //sets the status code 
-    res.status(err.status);
-    //pass in err object to give the template access to the error data
-    res.render('error', err); 
-});
-
-//message in the console
-    //if app point at a URL that doesn't exist as a router in the app ex: /error/error
-    app.use( (req, res, next) => {
-        //console log a user friendly message  
-        const err = new Error('oh noes!');
-        next(err);  
-    });    
