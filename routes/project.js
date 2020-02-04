f6219e73eeb254bef18cc6070470f7a7a07caae6
Akhaled19 <akhaled@clarku.edu>
2020-02-03 19:31:07 -0500

    updated the structure, reworded the comments, updated the route to simplified version, added two ways of using URL to posting data

--- routes/project.js
+++ routes/project.js
@@ -1,16 +1,16 @@
 //This file holds the project routes of my app 
 
-//missing: using the json file to render ids for each page 
-
 const express = require('express');
 const router = express.Router();
 const {projects} = require('../data.json');
 
-//set up a dynamic 'project' (/project) route 
-router.get('/projects', (req, res) => {
-    //render the 'project' page (see notes on treehouse)
-    res.render('project');
+//A dynamic 'project' route: If you go to a project without an id, it will send you back to the portfolio page with all projects 
+router.get('/', (req, res) => {
+    //render the 'project' page without an id 
+    res.render('/');
 }); 
+
+//Set up a dynamic 'project/id' using the json file to render ids for each page 
 
 
 //This allows us to export and use in app.js 
