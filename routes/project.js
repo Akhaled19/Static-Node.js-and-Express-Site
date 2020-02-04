fe5cd9e2fae379f3dc20ebc01379c1261d54942e
Akhaled19 <akhaled@clarku.edu>
2020-02-03 21:05:35 -0500

    built the structure for 'project/id'

--- routes/project.js
+++ routes/project.js
@@ -10,8 +10,21 @@ router.get('/', (req, res) => {
     res.render('/');
 }); 
 
-//Set up a dynamic 'project/id' using the json file to render ids for each page 
-
+//A dynamic 'project/id' using the json file to render ids for each page 
+//When the request is sent, it converts the projects object to an array 
+//then it renders the current project data to use in the pug template
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+    //Object.values() is a method that returns an array of a given object
+    const projectsArray = Object.values(projects);
+    //checks if the id matches up with any of project in the array
+    projectsArray.forEach(project => {
+        if(project.id === id ) {
+            const currentProject = 
+            res.render('project', currentProject);
+        }
+    });
+});
 
 //This allows us to export and use in app.js 
 module.exports = router;
