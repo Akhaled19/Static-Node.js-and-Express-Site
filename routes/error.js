4dc4701c6090222b716e23afaaa395eee011b62f
Akhaled19 <akhaled@clarku.edu>
2020-03-04 00:46:25 -0500

    added the stack (?)

--- routes/error.js
+++ routes/error.js
@@ -13,17 +13,18 @@ router.get( '*', (req, res, next) => {
     //display a user friendly message 
     const err = new Error('Could not find the requested resource.');
     err.status = 404;
+    console.log(err.stack);
     next(err);
 });    
 
                         /* Error Handler */                        
 router.use( (err, req, res, next) => { 
-    res.locals.err = err;
-    //reads the status property 
-    res.status(err.status);
-    console.log(`Error: ${err.message}`);
+    res.locals.message = err.message;
+    res.locals.status = err.status;
+    
+    console.error(err.stack);
     //pass in err object to give the template access to the error data
-    res.render('Error', {message: err});
+    res.render('error');
 });
 
 //export error handlers in order to use elsewhere  
